28466f065a25ba3d49649f53e5cad5a4c6184f26
Ale <alessandrochb@gmail.com>
2021-01-20 16:51:41 +0100

    add controller per la view della home + cambio forma rotta in web.php

--- resources/views/welcome.blade.php
+++
@@ -1,5 +0,0 @@
-@extends ('layouts.app')
-
-@section ('content')
-    <h1>HOME PAGE</h1>
-@endsection
--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('homePage');
+Route::get('/', 'HomeController@index')->name('homePage');
 
 Route::resource('/product','ProductController');
