89496654a226b8883c44da4ad1529f218f699dcc
Ale <alessandrochb@gmail.com>
2021-01-20 15:54:02 +0100

    add navbar bootstrap + creato link active con Request::route

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,23 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+        <a class="navbar-brand" href="#">DRESSYOURSELF</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample07" aria-controls="navbarsExample07" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarsExample07">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link {{ Request::route()->getName() == 'homePage' ? 'active' : '' }}" href="{{ route('homePage') }}">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link {{ Request::route()->getName() == 'product.index' ? 'active' : '' }}" href="{{ route('product.index') }}">Products</a>
+                </li>
+
+            </ul>
+            <form class="form-inline my-2 my-md-0">
+                <input class="form-control" type="text" placeholder="Search" aria-label="Search">
+            </form>
+        </div>
+    </div>
+</nav>
