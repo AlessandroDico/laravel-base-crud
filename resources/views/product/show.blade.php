e4c1a722210f808d13007e25b94f858374151872
Ale <alessandrochb@gmail.com>
2021-01-20 17:00:29 +0100

    aggiunto controllo su item che se non presente mette N.D

--- resources/views/product/show.blade.php
+++ resources/views/product/show.blade.php
@@ -11,27 +11,31 @@
                         </h5>
                         <h6 class="card-subtitle mb-2 text-muted">
                             Model :
-                            {{ $item->model}}
+                            {{-- {{ $item->model }} --}}
+                            {{ $item->model ? $item->model : 'N.D'}}
                         </h6>
                         <h6 class="card-subtitle mb-2 text-muted">
                             Fabric :
-                            {{ $item->fabric}}
+                            {{ $item->fabric ? $item->fabric : 'N.D'}}
                         </h6>
                         <h6 class="card-subtitle mb-2 text-muted">
                             Color :
-                            {{ $item->color}}
+                            {{-- {{ $item->color}} --}}
+                            {{ $item->color ? $item->color : 'N.D'}}
                         </h6>
                         <h6 class="card-subtitle mb-2 text-muted">
                             Size :
-                            {{ $item->size}}
+                            {{-- {{ $item->size }} --}}
+                            {{ $item->size ? $item->size : 'N.D' }}
                         </h6>
                         <h6 class="card-subtitle mb-2 text-muted">
                             Length :
-                            {{ $item->length}}
+                            {{ $item->length ? $item->length : 'N.D' }}
                         </h6>
                         <h6 class="card-subtitle mb-2 text-muted">
                             Price :
-                            {{ $item->price}} €
+                            {{-- {{ $item->price}} € --}}
+                            {{ $item->price ? $item->price : 'N.D' }} €
                         </h6>
                         <p>
                             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos accusantium porro ratione repellat reprehenderit natus a dignissimos fugit mollitia culpa? Accusantium earum error facilis tempore nisi, quidem animi incidunt at aut. Omnis laudantium rerum velit, praesentium possimus! Laboriosam, ratione, quaerat!
